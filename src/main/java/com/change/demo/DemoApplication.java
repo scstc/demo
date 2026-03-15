9efbde1134f59097926aa58dd995d09b45b6642b
simple <turing.sun@outlook.com>
2026-03-15 11:50:44 +0800

    update

--- src/main/java/com/change/demo/DemoApplication.java
+++ src/main/java/com/change/demo/DemoApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // 帮我写个除以0的异常
+        int a = 1;
+        int b = 0;
+        int c = a / b;
+        System.out.println(c);
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
